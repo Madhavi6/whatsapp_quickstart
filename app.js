66113a6f36f7bdb3725064c1349eb289285913a2
Madhavi6 <52055411+Madhavi6@users.noreply.github.com>
2023-06-11 19:57:51 +0200

    Add files via upload
    adding files to repository

---
+++ app.js
@@ -0,0 +1,26 @@
+import WhatsApp from 'whatsapp';
+
+// Your test sender phone number
+const wa = new WhatsApp('${process.env.WA_PHONE_NUMBER_ID}');
+
+// Enter the recipient phone number
+const recipient_number = '27655380421';
+
+async function send_message()
+{
+    try{
+        const sent_text_message = wa.messages.text( { "body" : "Hello world" }, recipient_number );
+
+        await sent_text_message.then( ( res ) =>
+        {
+            console.log( res.rawResponse() );
+        } );
+    }
+    catch( e )
+    {
+        console.log( JSON.stringify( e ) );
+    }
+}
+
+send_message();
+
